7388843335668076ff3478745d04593970b19970
Edsano <edsonmatheus02@hotmail.com>
2022-09-24 16:16:22 -0300

    alterações nos arquivos index.html, main.js e inclusão do style.css

---
+++ praticas-java/js/main.js
@@ -0,0 +1,26 @@
+/*
+EXERCÍCIO 001 - OLÁ, MUNDO!
+alert('Olá, mundo!')
+*/
+
+/*
+EXERCÍCIO 002 - POP-UP AO CLICAR NO BOTÃO
+function clicou() {
+    alert('Opa, tudo bom? Você clicou no botão!')
+}
+*/
+
+/*
+EXERCÍCIO 003 - USANDO PROMPT() E ALERT()
+var nome = prompt("Qual é o seu nome? ")
+alert("Olá " + nome +", É um prazer te conhecer!")
+
+OU
+
+function nome() {
+    let nome=prompt("Qual é o seu nome? ")
+    alert("Olá, ${nome}! É um prazer te conhecer!")
+}
+*/
+
+//EXERCÍCIO 004 - Interagindo com partes do HTML (DOM)
