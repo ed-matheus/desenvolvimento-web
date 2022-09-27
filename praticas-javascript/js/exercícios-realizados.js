3c79adc10f8e447c56cf0e8f0a4ca67aa364322b
Edsano <edsonmatheus02@hotmail.com>
2022-09-27 20:44:21 -0300

    alterações em index e main.js, criação do arquivo exercicios-realizados.js

---
+++ praticas-javascript/js/exercícios-realizados.js
@@ -0,0 +1,55 @@
+/*
+EXERCÍCIO 001 - OLÁ, MUNDO!
+alert('Olá, mundo!')
+*/
+
+/*
+//EXERCÍCIO 002 - POP-UP AO CLICAR NO BOTÃO
+function clicou() {
+    alert('Opa, tudo bom? Você clicou no botão!')
+}
+*/
+
+// EXERCÍCIO 003 - USANDO PROMPT() E ALERT()
+// var nome = prompt("Qual é o seu nome? ")
+// alert("Olá " + nome +", É um prazer te conhecer!")
+
+// OU
+
+// function inicio() {
+//     let nome = prompt("Qual é o seu nome? ")
+//     alert("Olá,"+ nome +"! É um prazer te conhecer!")
+// }
+
+//EXERCÍCIO 004 - Interagindo com partes do HTML (DOM)
+
+// function setReplace(frase, nome, novo_nome){
+//     return frase.replace(nome, novo_nome);
+// }
+
+// OU
+
+// FUNÇÃO RECURSIVA
+// function soma(n1){
+//     if(n1<=1){
+//         return 1;
+//     }
+//     else{
+//         return n1 + soma(n1-1);
+//     }
+// }
+
+// alert(soma(10,10));
+// alert(setReplace("Gosto de formiga","formiga","pão"));
+
+// EXERCÍCIO 005 - Faça um programa que leia 3 números e, para cada um, imprimir o dobro.
+// function dobro(num){
+//     return num*2
+// }
+// var n1 = prompt('Entre com o primeiro número: ')
+// var n2 = prompt('Entre com o segundo número: ')
+// var n3 = prompt('Entre com o terceiro número: ')
+
+// console.log("O dobro de "+ n1 +" é: " + dobro(n1))
+// console.log("O dobro de "+ n2 +" é: " + dobro(n2))
+// console.log("O dobro de "+ n3 +" é: " + dobro(n3))
