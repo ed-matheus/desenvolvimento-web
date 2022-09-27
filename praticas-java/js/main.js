e8fce55007ca37a12b9746c4fe8430b21d2604db
Edsano <edsonmatheus02@hotmail.com>
2022-09-26 23:00:17 -0300

    alterações em index.html, main.js e style.css

--- praticas-java/js/main.js
+++ praticas-java/js/main.js
@@ -4,23 +4,42 @@ alert('Olá, mundo!')
 */
 
 /*
-EXERCÍCIO 002 - POP-UP AO CLICAR NO BOTÃO
+//EXERCÍCIO 002 - POP-UP AO CLICAR NO BOTÃO
 function clicou() {
     alert('Opa, tudo bom? Você clicou no botão!')
 }
 */
 
-/*
-EXERCÍCIO 003 - USANDO PROMPT() E ALERT()
-var nome = prompt("Qual é o seu nome? ")
-alert("Olá " + nome +", É um prazer te conhecer!")
+// EXERCÍCIO 003 - USANDO PROMPT() E ALERT()
+// var nome = prompt("Qual é o seu nome? ")
+// alert("Olá " + nome +", É um prazer te conhecer!")
 
-OU
+// OU
 
-function nome() {
-    let nome=prompt("Qual é o seu nome? ")
-    alert("Olá, ${nome}! É um prazer te conhecer!")
-}
-*/
+// function inicio() {
+//     let nome = prompt("Qual é o seu nome? ")
+//     alert("Olá,"+ nome +"! É um prazer te conhecer!")
+// }
 
 //EXERCÍCIO 004 - Interagindo com partes do HTML (DOM)
+
+
+// FUNÇÃO RECURSIVA
+// function soma(n1){
+//     if(n1<=1){
+//         return 1;
+//     }
+//     else{
+//         return n1 + soma(n1-1);
+//     }
+// }
+
+// function setReplace(frase, nome, novo_nome){
+//     return frase.replace(nome, novo_nome);
+// }
+
+// alert(soma(10,10));
+// alert(setReplace("Gosto de formiga","formiga","pão"));
+
+// EXERCÍCIO 005
+// Faça um programa que leia 3 números e, para cada um, imprimir o dobro.
