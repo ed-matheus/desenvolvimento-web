3048add34e05b75c74c76fe80fc805cfa64d5d55
Edsano <edsonmatheus02@hotmail.com>
2022-10-22 17:12:00 -0300

    progresso no projeto clone-google, criacao do arquivo js

---
+++ Projetos/Projetos-Autonomos/clone-google/js/dark-mode.js
@@ -0,0 +1,12 @@
+// let change = document.getElementById('tudo');
+
+// // alterações na página
+// change.style.backgroundColor = '#1f1f1f';
+// change.style.
+
+var clique = document.getElementById("botao-dark");
+var dark = document.getElementById("botao-dark");
+
+clique.onclick = function () {
+    document.dark.style.backgroundColor = "#1f1f1f";
+};
