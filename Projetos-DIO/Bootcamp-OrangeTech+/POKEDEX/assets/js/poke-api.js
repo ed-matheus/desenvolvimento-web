0d964e62219f7263a98496b6fb6fb29557c84dc4
Edsano <edsonmatheus02@hotmail.com>
2022-12-10 18:20:21 -0300

    criação do arquivo pokemon-model.js e progresso nos demais arquivos do projeto POKEDEX

--- Projetos-DIO/Bootcamp-OrangeTech+/POKEDEX/assets/js/poke-api.js
+++ Projetos-DIO/Bootcamp-OrangeTech+/POKEDEX/assets/js/poke-api.js
@@ -1,11 +1,30 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = 'https://pokeapi.co/api/v2/pokemon?offset=' + offset + '&limit=' + limit;
 
     return fetch(url)
---
+++ Projetos-DIO/Bootcamp-OrangeTech+/POKEDEX/assets/js/pokemon-model.js
@@ -0,0 +1,10 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+}
+
+
