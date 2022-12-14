6207b6fd285bb2ab1a5251f1a57c633c005d19fe
Edsano <edsonmatheus02@hotmail.com>
2022-12-14 20:18:50 -0300

    alterações em desafio de código

--- Projetos-e-Desafios-DIO/Bootcamp-OrangeTech+/Desafios-de-Codigo/somando-multiplos.js
+++ Projetos-e-Desafios-DIO/Bootcamp-OrangeTech+/Desafios-de-Codigo/somando-multiplos.js
@@ -1,24 +1,21 @@
-
 // Dado um número A e o seu limite N, encontre a soma de todos os múltiplos A até o seu limite N.
 
-const a = 3;
-const N = 18;
+const a = 5;
+const N = 20;
 const numList = [];
 let soma = 0;
 
-for (let i = 1; i < N; i++) {
+for (let i = 1; i < 10+1; i++) {
     const mult = i * a;
     numList.push(mult);
 
     if (mult == N) {
         break;
     }
-
 }
 
 for (let i = 0; i < numList.length; i++) {
     soma += numList[i];
-
 }
 
 console.log(soma);
