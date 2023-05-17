2bf1c5ac8f3b32e747ae003a7e44e6ab384e09a7
Edsano <edsonmatheus02@hotmail.com>
2023-05-17 19:17:36 -0300

    alterações no projeto clone do google, tentando enfim implementar o modo escuro na página

---
+++ Projetos/Autonomos/google-clone/js/script.js
@@ -0,0 +1,13 @@
+
+function darkMode() {
+    const body = document.getElementById('body');
+    const changeModeButton = document.getElementById('changeModeButton');
+    const menu = document.getElementById('menu');
+
+    body.style.backgroundColor = '#252525'
+
+    changeModeButton.style.backgroundColor = '#252525'
+    changeModeButton.style.color = '#fff'
+
+    menu.style.color = '#fff'
+}
